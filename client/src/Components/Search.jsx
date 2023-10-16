4f7f33fa2cdca3971ebb406e01202ebd0cdae460
MokshagnaV <mokshagnavemula@gmail.com>
2023-10-16 14:14:23 +0530

    Resolve Merge conflicts

--- client/src/Components/Search.jsx
+++ client/src/Components/Search.jsx
@@ -1,73 +1,22 @@
-import React, { useState, useEffect } from "react";
-import Autosuggest from "react-autosuggest";
+import React, { useState } from "react";
+import AsyncSelect from "react-select/async";
+import { toast } from "react-toastify";
 import axios from "axios";
 
 const Search = ({ onSearch, onFilterChange, onSortChange }) => {
-  const [value, setValue] = useState("");
-  const [suggestions, setSuggestions] = useState([]);
-  const [loading, setLoading] = useState(false);
   const [selectedFilter, setSelectedFilter] = useState("title");
   const [selectedSort, setSelectedSort] = useState("relevance");
 
   const filters = ["title", "author", "subject", "published"];
   const sortOptions = ["relevance", "newest", "oldest"];
 
-  useEffect(() => {
-    const getSuggestions = async (inputValue) => {
-      try {
-        if (inputValue.trim() === "") {
-          // If inputValue is empty, clear suggestions and return
-          setSuggestions([]);
-          return;
-        }
+  // used to check if one toast is already displaying and prevent multiple toasts
+  const toastId = React.useRef(null);
 
-        setLoading(true);
-
-        const response = await axios.get(
-          `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
-            inputValue
-          )}`
-        );
-
-        if (response.data.items) {
-          const books = response.data.items.map((book) => ({
-            id: book.id,
-            title: book.volumeInfo.title,
-            author: book.volumeInfo.authors
-              ? book.volumeInfo.authors.join(", ")
-              : "Unknown",
-            subject: book.volumeInfo.categories
-              ? book.volumeInfo.categories.join(", ")
-              : "Unknown",
-            published: book.volumeInfo.publishedDate || "Unknown",
-          }));
-          setSuggestions(books);
-        }
-      } catch (error) {
-        console.error("Error fetching suggestions:", error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
-    getSuggestions(value);
-  }, [value]);
-
-  const onSuggestionsFetchRequested = ({ value }) => {
-    // No need to fetch suggestions here, it's now handled in the useEffect
-  };
-
-  const onSuggestionsClearRequested = () => {
-    setSuggestions([]);
-  };
-
-  const getSuggestionValue = (suggestion) => suggestion.title;
-
-  const renderSuggestion = (suggestion) => <div>{suggestion.title}</div>;
-
-  const onChange = (event, { newValue }) => {
-    setValue(newValue);
-    onSearch(newValue);
+  const notifyerror = (msg) => {
+    if (!toast.isActive(toastId.current)) {
+      toastId.current = toast.error(msg);
+    }
   };
 
   const handleFilterChange = (event) => {
@@ -80,36 +29,58 @@ const Search = ({ onSearch, onFilterChange, onSortChange }) => {
     onSortChange(event.target.value);
   };
 
-  const inputProps = {
-    placeholder: "Search for books",
-    value,
-    onChange,
-    className:
-    "w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-[#523f1b] text-gray-700 bg-white placeholder-gray-400 shadow-sm transition-all duration-300 ease-in-out",
-  };
+  function handleSelect(input) {
+    onSearch(input.label);
+  }
+
+  function handleChange(input) {
+    if (!input) return [];
+    onSearch(input);
+    return axios
+      .get(
+        `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(
+          input
+        )}`
+      )
+      .then((res) => {
+        const data = res.data;
+        const opts = data.items.map((d) => {
+          return {
+            value: d.id,
+            label: `${d.volumeInfo.title}${
+              d.volumeInfo.publisher ? `, ${d.volumeInfo.publisher}` : ""
+            }`,
+          };
+        });
+        return opts;
+      })
+      .catch((err) => {
+        notifyerror(err.message);
+        return [];
+      });
+  }
+
   return (
     <div className="container mx-auto p-4">
-      <div className="lg:flex justify-between items-center mb-4">
-        <Autosuggest
-          suggestions={suggestions}
-          onSuggestionsFetchRequested={onSuggestionsFetchRequested}
-          onSuggestionsClearRequested={onSuggestionsClearRequested}
-          getSuggestionValue={getSuggestionValue}
-          renderSuggestion={renderSuggestion}
-          inputProps={inputProps}
+      <div className="lg:flex justify-between items-center gap-5 mb-4">
+        <AsyncSelect
+          placeholder="Search for Books Suggestions"
+          loadOptions={handleChange}
+          onChange={handleSelect}
+          defaultOptions
+          className="w-full"
         />
-        <div className="ml-4 mt-4 gap-4 grid lg:flex lg:gap-8 lg:m-0">
-          {" "}
+        <div className="mt-4 gap-4 grid lg:flex lg:m-0">
           {/* Added ml-4 here */}
-          <div className="relative flex items-center justify-between gap-2">
-            <label htmlFor="filter" className="text-gray-600">
+          <div className="relative flex items-center justify-between gap-2 w-full">
+            <label htmlFor="filter" className="text-gray-600 whitespace-nowrap">
               Filter By :{" "}
             </label>
             <select
               id="filter"
               value={selectedFilter}
               onChange={handleFilterChange}
-              className="border rounded-lg px-1 py-1 pr-4 focus:outline-none focus:border-blue-500 shadow-sm "
+              className="px-4 py-2 capitalize border border-slate-300 bg-white rounded-lg focus:outline-none focus:border-blue-500 shadow-sm  max-md:w-full"
             >
               {filters.map((filter) => (
                 <option key={filter} value={filter}>
@@ -118,15 +89,15 @@ const Search = ({ onSearch, onFilterChange, onSortChange }) => {
               ))}
             </select>
           </div>
-          <div className="relative flex items-center justify-between gap-2">
-            <label htmlFor="sort" className="text-gray-600">
+          <div className="relative flex items-center justify-between gap-2 ">
+            <label htmlFor="sort" className="text-gray-600 whitespace-nowrap">
               Sort By :{" "}
             </label>
             <select
               id="sort"
               value={selectedSort}
               onChange={handleSortChange}
-              className="border rounded-lg px-2 py-1 pr-4 focus:outline-none focus:border-blue-500 shadow-sm"
+              className="px-4 py-2 capitalize border border-slate-300 bg-white rounded-lg focus:outline-none focus:border-blue-500 shadow-sm max-md:w-full"
             >
               {sortOptions.map((sortOption) => (
                 <option key={sortOption} value={sortOption}>
@@ -134,12 +105,10 @@ const Search = ({ onSearch, onFilterChange, onSortChange }) => {
                 </option>
               ))}
             </select>
-            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-600">
-            </div>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-600"></div>
           </div>
         </div>
       </div>
-      {loading && <p>Loading suggestions...</p>}
     </div>
   );
 };

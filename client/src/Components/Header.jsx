4ffd4b7368c7b6af193e0923dc50865d12b2e87b
Ayusht777 <talesarayush@gmail.com>
2023-10-20 19:21:00 +0530

    Update the UI of Navbar

--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -5,17 +5,19 @@ import { useCart } from "../utils/CartContext";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Logo from "./Logo";
+// import Logo from "./Logo";
 
+import { FiLayers, FiShoppingCart } from "react-icons/fi";
 const Header = () => {
-  const { loginWithRedirect, isAuthenticated,  logout, user } = useAuth0();
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
   const { cartItems } = useCart();
   const navigate = useNavigate();
   const isTrue = true; // Variable to temporarily store true value
 
   // Function to navigate to the cart page if logged in, show prompt if not
   const handleCartClick = () => {
-    if (isTrue) { //Change to isAuthenticated to enable authentication
+    if (isTrue) {
+      //Change to isAuthenticated to enable authentication
       navigate("/cart");
     } else {
       toast.error("Please log in to access the cart.", {
@@ -28,72 +30,63 @@ const Header = () => {
   };
 
   return (
-    <header className="bg-[#523f1b] p-4 shadow-lg">
-      <div className="container mx-auto flex justify-between items-center">
+    <header className=" p-4 box-border">
+      <div className="w-full h-11 box-border flex justify-between items-center bg-gradient-to-l from-gray-50 to-white md:px-6 px-3 py-1 rounded-md shadow-[rgba(0,_0,_0,_0.24)_0px_3px_8px] ">
         {/* Brand Logo as a Link with Bounce Animation */}
-        <Link
-          to="/"
-          className="text-white text-3xl font-semibold tracking-tight flex items-center"
-          
-        >
-          <Logo/>
+        <Link to="/" className="tracking-tight pl-4">
+          <div className="flex justify-center items-center relative">
+            <div className="p-2 box-border rounded-full bg-[#E9DBD1] absolute z-10">
+              <FiLayers className=" -rotate-[40deg] text-xl text-[#523F1B]"></FiLayers>
+            </div>
+            <div className="bg-[#E9DBD1]  rounded-full absolute left-0 w-28 flex justify-end items-center h-[1.65rem] box-border px-4">
+              <p className=" font-semibold  text-[#523F1B] box-border ">
+                My Library
+              </p>
+            </div>
+          </div>
         </Link>
-<div className="content flex max-[500px]:flex-col-reverse flex-row items-center gap-4 ">
-  {/* Display user name if authenticated */}
+        <div className="content flex max-[500px]:flex-col-reverse flex-row items-center gap-4 ">
+          {/* Display user name if authenticated */}
           {isAuthenticated && (
             <span className="text-white text-sm font-medium">
               Hello, {user.name}
             </span>
           )}
-  
-        {/* Navigation */}
-        <nav className="space-x-4 flex items-center">
-          
 
-          {/* Login/Logout Button */}
-          {isAuthenticated ? (
-            <button
-              className="text-white  border border-white hover:bg-[#ead9c6] hover:text-[#523f1b] font-semibold py-2 px-4 rounded-full transition duration-300 ease-in-out"
-              onClick={() => logout({ returnTo: window.location.origin })}
-            >
-              Log Out
-            </button>
-          ) : (
-            <button
-              className="text-white  border border-white hover:bg-[#ead9c6] hover:text-[#523f1b] font-semibold py-2 px-4 rounded-full transition duration-300 ease-in-out"
-              onClick={() => loginWithRedirect()}
-            >
-              Log In
-            </button>
-          )}
-
-          {/* Cart Button with Hover Animation */}
-          <button
-            className="relative text-white hover:text-blue-500 transform transition-transform duration-300 ease-in-out hover:scale-105"
-            onClick={handleCartClick}
-          >
-            {/* New Cart SVG */}
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="32"
-              height="32"
-              viewBox="0 0 48 48"
-            >
-              <path
-                fill="#ffffff" // Fill color set to white
-                d="M14 36c-2.21 0-3.98 1.79-3.98 4s1.77 4 3.98 4 4-1.79-4-4-1.79-4-4-4zM2 4v4h4l7.19 15.17-2.7 4.9c-.31.58-.49 1.23-.49 1.93 0 2.21 1.79 4 4 4h24v-4H14.85c-.28 0-.5-.22-.5-.5 0-.09.02-.17.06-.24L16.2 26h14.9c1.5 0 2.81-.83 3.5-2.06l7.15-12.98c.16-.28.25-.61.25-.96a2 2 0 0 0-2-2H10.43l-1.9-4H2zm32 32c-2.21 0-3.98 1.79-3.98 4s1.77 4 3.98 4 4-1.79-4-4-1.79-4-4-4z"
-              ></path>
-            </svg>
-
-            {isTrue && ( //Change to isAuthenticated to enable authentication
-              <span className="absolute top-0 right-0 bg-red-500 text-white w-4 h-4 rounded-full flex items-center justify-center text-xs font-semibold">
-                {cartItems.length}
-              </span>
+          {/* Navigation */}
+          <nav className="space-x-6 flex items-center">
+            {/* Login/Logout Button */}
+            {isAuthenticated ? (
+              <button
+                className="text-[#222222]  border-2 border-[#523F1B] hover:bg-[#ead9c6] hover:text-[#523f1b]  font-semibold py-[.15rem] px-4 rounded-md transition duration-300 ease-in-out flex justify-center items-center"
+                onClick={() => logout({ returnTo: window.location.origin })}
+              >
+                Log Out
+              </button>
+            ) : (
+              <button
+                className="text-[#222222]  border-2 border-[#523F1B] hover:bg-[#ead9c6] hover:text-[#523f1b]  font-semibold py-[.15rem] px-4 rounded-md transition duration-300 ease-in-out flex justify-center items-center"
+                onClick={() => loginWithRedirect()}
+              >
+                Log In
+              </button>
             )}
-          </button>
-        </nav>
-        
-</div>
+
+            {/* Cart Button with Hover Animation */}
+            <button
+              className="relative  text-[#523F1B] group   transform transition-transform duration-300 ease-in-out hover:scale-105 "
+              onClick={handleCartClick}
+            >
+              {/* New Cart SVG */}
+              <FiShoppingCart className="text-3xl font-semibold group-hover:text-[#ead9c6]"></FiShoppingCart>
+              {isTrue && ( //Change to isAuthenticated to enable authentication
+                <span className="absolute top-0 right-0 bg-gray-400 text-white w-4 h-4 rounded-full flex items-center justify-center text-xs font-semibold group-hover:bg-gray-600">
+                  {cartItems.length}
+                </span>
+              )}
+            </button>
+          </nav>
+        </div>
       </div>
     </header>
   );
